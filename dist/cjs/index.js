e128792dda21ccd5b1716b313026b7d5ec1433d0
fofx <identityofsine@gmail.com>
2024-03-28 18:12:12 -0400

    fixed imports

---
+++ dist/cjs/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Draggable = void 0;
+var tslib_1 = require("tslib");
+var drgndrp_1 = tslib_1.__importDefault(require("./drgndrp"));
+exports.Draggable = drgndrp_1.default;
+//# sourceMappingURL=index.js.map
